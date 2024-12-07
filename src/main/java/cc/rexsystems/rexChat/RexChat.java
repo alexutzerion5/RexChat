4c2df953676e388fbb93a79d43c04edfc610121a
alexutzerion5 <alexutzeranio@gmail.com>
2024-12-07 18:08:07 +0200

    t

---
+++ src/main/java/cc/rexsystems/rexChat/RexChat.java
@@ -0,0 +1,17 @@
+package cc.rexsystems.rexChat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RexChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
